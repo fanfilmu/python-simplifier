00ac7017c8e025c24e35542eebb366f7a9989e59
fanfilmu <michalb@student.agh.edu.pl>
2015-01-24 16:46:25 +0100

    Add removing while instructions with condition known to be false

--- src/main/scala/simplifier/simplifier.scala
+++ src/main/scala/simplifier/simplifier.scala
@@ -6,5 +6,22 @@ import AST._
 // avoid one huge match of cases
 // take into account non-greedy strategies to resolve cases with power laws
 object Simplifier {
-  def simplify(node: Node) = node
+  def simplify(node: Node) : Node = {
+    node match {
+      case n@NodeList(_) => simplify_node_list(n)
+      case n@WhileInstr(_,_) => simplify_while_instr(n)
+      case _ => node
+    }
+  }
+
+  def simplify_node_list(listNode: NodeList) : Node = (
+    new NodeList(listNode.list.map(x => simplify(x)).filter(_ != null))
+  )
+
+  def simplify_while_instr(loop: WhileInstr) : Node = (
+    simplify(loop.cond) match {
+      case FalseConst() => null
+      case _ => WhileInstr(simplify(loop.cond), simplify(loop.body))
+    }
+  )
 }
